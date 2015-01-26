3a77970b8a98fb251d72a5b89dea88f67bdf1784
tengzhaoqi2012 <tengzhaoqi2012@126.com>
2015-01-26 16:09:17 +0800

    some message

--- bin/myshell1.sh
+++ bin/myshell1.sh
@@ -10,6 +10,8 @@ date1=$(date +%Y%m%d)
 date2=$(date -d "+1 day" +%Y%m%d)
 date3=$(date -d "+2 day" +%Y%m%d)
 
+cd sql
+
 touch ${tempName}${date1}.sql
 touch ${tempName}${date2}.sql
 touch ${tempName}${date3}.sql
