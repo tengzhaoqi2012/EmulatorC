d8e3f6c2d2ec620cce9ecef2a6f45e559d1a2f92
tengzhaoqi2012 <tengzhaoqi2012@126.com>
2015-01-26 14:02:40 +0800

    some message

--- bin/myshell1.sh
+++ bin/myshell1.sh
@@ -1,2 +1,15 @@
 #/bin/bash
-echo "hello world!"
+PATH=$PATH
+export PATH
+
+echo "程序会用touch命令自动创建三个文件"
+
+read -p "请输入文件名的前缀:" tempName
+
+date1=$(date +%Y%m%d)
+date2=$(date -d "+1 day" +%Y%m%d)
+date3=$(date -d "+2 day" +%Y%m%d)
+
+touch ${tempName}${date1}.sql
+touch ${tempName}${date2}.sql
+touch ${tempName}${date3}.sql
