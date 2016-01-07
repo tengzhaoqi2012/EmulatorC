eb02b0711a02bf8d2669d59df39da9b7221f1c7a
tengzhaoqi2012 <tengzhaoqi2012@126.com>
2016-01-07 20:12:27 +0800

    test

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include "c5.h"
 #include "calc.h"
 int main(){
-	printf("Hello World!!!\n");
-	func5();
-	int c = calc(23, 35);
+	printf("please select:\n");
+	printf("1:test1\n");
+	printf("2:test2\n");
+	//func5();
+	//int c = calc(23, 35);
 }
